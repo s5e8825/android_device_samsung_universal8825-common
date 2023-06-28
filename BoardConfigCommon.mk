434333573ca13a91a84c90f3d855092064e51f89
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2023-06-28 23:14:33 +0400

    universal8825-common: add device framework matrices

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,10 @@ TARGET_KEYMASTER_VARIANT := samsung
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
